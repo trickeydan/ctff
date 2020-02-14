e65bf843f1c9173bc58efdf8eaf72ec2eca3cb63
Dan Trickey <dan@trickey.io>
2020-02-14 13:33:44 +0000

    Remove types.py

--- ctff/types.py
+++
@@ -1,8 +0,0 @@
-"""Common type variables."""
-from typing import TypeVar
-
-from .challenge.challenge import Challenge
-from .views.challenge import ChallengeView
-
-ChallengeT = TypeVar("ChallengeT", bound=Challenge)
-ChallengeViewT = TypeVar("ChallengeViewT", bound=ChallengeView)
